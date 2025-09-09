6343208d546d4dbfd1c152840c95d19a6a431150
Denis Golikov <denis.golikov@improvado.io>
2025-09-09 11:25:31 +0500

    mart_client_drawdown_analysis

--- models/facts/fact_account_eod.sql
+++ models/facts/fact_account_eod.sql
@@ -1,14 +1,20 @@
--- Fact table for end-of-day account balances - passthrough from staging
--- No additional business rules applied beyond staging layer
+-- Fact table for end-of-day account balances with client linkage
+-- Business improvements:
+-- 1. Link accounts to clients using dim_account
 
 with staging_balances_eod as (
     select * from {{ ref('stg_balances_eod') }}
+),
+
+account_dimension as (
+    select * from {{ ref('dim_account') }}
 ),
 
 final_fact_account_eod as (
     select
         -- Primary identifiers
         account_id,
+        client_id,
         platform,
         date,
         
@@ -23,7 +29,9 @@ final_fact_account_eod as (
         _loaded_at,
         current_timestamp() as _transformed_at
         
-    from staging_balances_eod
+    from staging_balances_eod b
+    left join account_dimension a
+        on b.account_id = a.account_id
 )
 
 select * from final_fact_account_eod
