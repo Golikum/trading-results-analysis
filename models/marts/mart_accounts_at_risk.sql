e51b083507bb2c3eb587db52618df3bbeced8f10
Denis Golikov <denis.golikov@improvado.io>
2025-09-09 12:09:25 +0500

    mart_accounts_at_risk

---
+++ models/marts/mart_accounts_at_risk.sql
@@ -0,0 +1,26 @@
+{{ config(
+    materialized='table',
+    schema='trading_marts'
+) }}
+
+-- Account risk analysis
+-- Business question: Which clients/accounts are at risk
+-- Accounts marked is_deleted = true but still trading.
+
+with fact_trades as (
+    select * from {{ ref('fact_trades') }}
+)
+
+select 
+    any_value(client_id)                                    as client_id,
+    account_id,
+    count(trade_id)                                         as trades_number,
+    if(sum(cast(account_is_deleted as INT64)) > 0, 1, 0)    as is_account_deleted
+from
+    trades_data
+group by
+    account_id
+having
+    is_account_deleted = 1
+order by
+    client_id
